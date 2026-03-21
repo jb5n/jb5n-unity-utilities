f06cf325d87a16654e702e13abf05ff609220d37
Justin Bostian <jrbostian@gmail.com>
2026-03-21 17:34:34 -0500

    Add component utils

---
+++ Runtime/ComponentUtils.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System;
+using System.Reflection;
+
+public static class ComponentUtils
+{
+	// Taken from https://discussions.unity.com/t/how-to-get-a-component-from-an-object-and-add-it-to-another-copy-components-at-runtime/80939/4
+	public static T GetCopyOf<T>(this Component comp, T other) where T : Component
+	{
+		Type type = comp.GetType();
+		if (type != other.GetType()) return null; // type mis-match
+		BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+		PropertyInfo[] pinfos = type.GetProperties(flags);
+		foreach (var pinfo in pinfos)
+		{
+			if (pinfo.CanWrite)
+			{
+				try
+				{
+					pinfo.SetValue(comp, pinfo.GetValue(other, null), null);
+				}
+				catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+			}
+		}
+		FieldInfo[] finfos = type.GetFields(flags);
+		foreach (var finfo in finfos)
+		{
+			finfo.SetValue(comp, finfo.GetValue(other));
+		}
+		return comp as T;
+	}
+
+	public static T AddComponent<T>(this GameObject go, T toAdd) where T : Component
+	{
+		return go.AddComponent<T>().GetCopyOf(toAdd) as T;
+	}
+}
